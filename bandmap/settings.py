9004d6b1aea1a0c77e55e1db465f676df1006721
xxristoskk <katsaros.christopher@gmail.com>
2021-04-16 07:59:03 -0400

    changed static root

--- bandmap/settings.py
+++ bandmap/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'bandmap/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'bandmap/static')
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'bandmap/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'bandmap/static')
