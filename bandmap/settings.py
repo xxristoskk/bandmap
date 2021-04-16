0f6ed3ed96ab3b2f4037dd9e1743e4c11c5707c9
xxristoskk <katsaros.christopher@gmail.com>
2021-04-16 08:01:50 -0400

    changed allowed hosts

--- bandmap/settings.py
+++ bandmap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bandmap-v1.herokuapp.com']
 
 
 # Application definition
