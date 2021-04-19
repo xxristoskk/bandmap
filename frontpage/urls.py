c0e7550a4bd5b136165e8722d807493800f9a24a
xxristoskk <katsaros.christopher@gmail.com>
2021-04-19 18:35:11 -0400

    added view to urls

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MapView
-from django.views.decorators.csrf import ensure_csrf_cookie
+from .views import MapView, SaveLocation
 
 app_name = 'frontpage'
 
 urlpatterns = [
-    path('', ensure_csrf_cookie(MapView.as_view()), name='map'),
+    path('', MapView.as_view(), name='map'),
     path('save_location/', SaveLocation.as_view())
 ]
