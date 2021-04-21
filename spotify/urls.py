e03ab87666e3a0ac707d96c6afbcd67b21553be0
xxristoskk <katsaros.christopher@gmail.com>
2021-04-21 02:47:24 -0400

    added spotify feature

--- spotify/urls.py
+++ spotify/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import AuthURL, callback, PlaylistAPI
+from .views import AuthURL, callback, PlaylistMaker
 
 urlpatterns = [
     path('auth/', AuthURL.as_view()),
     path('redirect/', callback),
-    path('make_playlist/', PlaylistAPI)
+    path('make_playlist/', PlaylistMaker.as_view())
 ]
