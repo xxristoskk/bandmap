564dceacb059a2e26fe7abf0e717e5eb4a5ec5eb
xxristoskk <katsaros.christopher@gmail.com>
2022-03-07 20:48:40 +0000

    pythonanywhere update

--- bandmap/settings.py
+++ bandmap/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -138,8 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'bandmap/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'bandmap/static')
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'bandmap/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'bandmap/static')
