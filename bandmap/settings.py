3dfd1fb13f5b423f828c3f1375550870ead74b64
xxristoskk <katsaros.christopher@gmail.com>
2022-07-20 19:08:45 -0400

    static

--- bandmap/settings.py
+++ bandmap/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 # ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'bandmap/static/')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
