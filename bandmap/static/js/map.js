5bdd1141d495cbc30e97059c3c7d5f1c6f8c6c93
xxristoskk <katsaros.christopher@gmail.com>
2022-07-20 12:55:36 -0400

    big update

--- bandmap/static/js/map.js
+++ bandmap/static/js/map.js
@@ -1,67 +1,53 @@
 /*
 TABLE OF CONTENTS:
 1a. LOCATION INITIALIZATION
-2a. BUTTON EVENTS
-3a. MAP
-4a. MAP EVENTS
+2a. OPTIONS INITIALIZATION
+3a. MENU BUTTON EVENTS
+4a. MAP INITIALIZATION
+5a. MAP EVENTS
 */
 
+
 // 1a
-// INITIALIZE LOCATION FROM COOKIES
-currentLocation = JSON.parse(document.getElementById('loc').textContent)
+// INITIALIZE LOCATION
+first_visit = JSON.parse(document.getElementById('first_visit').textContent)
 
-// CHECK FOR GEOLOCATION API
-if (!navigator.geolocation) {
-  console.error(`Your browser doesn't support Geolocation`)
-};
+if (first_visit) {
+  document.querySelector('.popup-intro').classList.add('activate')
+  document.querySelector('.popup-intro-content').classList.add('activate')
+  document.querySelector('#save-data').addEventListener('click', function() {
 
-// LOCATION FUNCTIONS
-function getLocation() {
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(saveLocation)
-} else {
-  document.querySelector('#message').textContent = "Browser doesn't support Geolocation";
-  }
-};
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
+    var chipsInstance = M.Chips.getInstance(document.querySelector('.chips'))
+    var user_location = document.querySelector('#location').value
 
-function saveLocation(position) {
-  // PRELOAD ANIMATION HERE
-  document.querySelector(".preload").classList.add('activate')
-  const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-  const data = {
-    'longitude': position.coords.longitude,
-    'latitude': position.coords.latitude,
-  }
-
-  fetch('save_location/', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {'X-CSRFToken': csrftoken, 'content-type':'application/json'}
-  })
-  .then(data => {
-    
-    console.log('Fetch is made')
-    if (data.status == '200' && currentLocation.latitude == null) {
-      // remove preload animation
-      document.querySelector('.preload').classList.remove('activate')
-
-      // open popup
-      document.querySelector('.popup-intro').classList.add('activate')
-      document.querySelector('.popup-intro-content').classList.add('activate')
-      
-    } else {
-      alert('There was an error! Try refreshing the page')
+    var optionsData = {
+      location: user_location,
+      genres: chipsInstance.chipsData
     }
-  })
-};
 
-// CALL LOCATION FUNCTIONS IF NONE IN COOKIES
-if (currentLocation.latitude == null) {
-  getLocation()
+    document.querySelector(".preload").classList.add('activate')
+
+    fetch('',{
+      method: 'POST',
+      body: JSON.stringify(optionsData),
+      headers: {'X-CSRFToken': csrftoken, 'content-type':'application/json'}
+    })
+    .then(data => {
+      if (data.status == '200') {
+        location.reload()
+        document.querySelector(".preload").classList.remove('activate')
+      }
+    })
+  })
+} else {
+  currentLocation = JSON.parse(document.getElementById('loc').textContent)
 }
 
+
 // 2a
-/* BUTTONS */
+/* MENU BUTTONS */
+
 // toggle new searchbar
 document.querySelector('#addSearch').addEventListener('click', function() {
   if (map.hasControl(searchBar)) {
@@ -71,12 +57,14 @@ document.querySelector('#addSearch').addEventListener('click', function() {
   }
 })
 
-// POPULATE NEW LOCATION
+// POPULATE NEW MAP FROM NEW LOCATION
 document.querySelector('#newLocation').addEventListener('click', function() {
+
   const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
   let newCenter = map.getCenter();
   const data = {'new_location': newCenter}
   document.querySelector(".preload").classList.add('activate')
+
   fetch('new_location/',{
     method: 'POST',
     body: JSON.stringify(data),
@@ -84,7 +72,7 @@ document.querySelector('#newLocation').addEventListener('click', function() {
   })
   .then(data => {
     if (data.status == '200') {
-      setTimeout(function(){
+      setTimeout(function() {
         document.querySelector(".preload").classList.remove('activate')
         location.reload()
       }, 700);
@@ -107,14 +95,57 @@ if (document.body.contains(document.querySelector('#spotifyAuth'))) {
 /* MAP SETUP */
 var optionDark = document.querySelector("[value='mapbox://styles/mapbox/dark-v10']")
 var optionLight = document.querySelector("[value='mapbox://styles/mapbox/light-v10']")
+
+// check local storage for saved map style
 if (!(localStorage.getItem('mapStyle'))) {
   localStorage.setItem('mapStyle', 'mapbox://styles/mapbox/dark-v10')
   var mapStyle = localStorage.getItem('mapStyle')
+  optionDark.setAttribute('selected','selected')
+  optionLight.removeAttribute('selected','selected')
 } else {
   var mapStyle = localStorage.getItem('mapStyle')
 }
 
-// INITIALIZE MAP OPTIONS
+if (document.body.contains(optionLight) || document.body.contains(optionDark)){
+  // make sure the current style is the default selection
+  if (mapStyle == 'mapbox://styles/mapbox/light-v10') {
+    optionLight.setAttribute('selected','selected')
+    optionDark.removeAttribute('selected','selected')
+  } else {
+    optionLight.removeAttribute('selected','selected')
+    optionDark.setAttribute('selected','selected')
+  }
+}
+
+// OPTIONS (genre filter, mode, styles)
+if (document.body.contains(document.querySelector('#options'))) {
+
+  // saving the changes
+  document.querySelector('#options').addEventListener('click', function() {
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
+    var chipsInstance = M.Chips.getInstance(document.querySelector('.chips'))
+    var optionsData = {
+      location: currentLocation,
+      genres: chipsInstance.chipsData
+    }
+    document.querySelector(".preload").classList.add('activate')
+
+
+    fetch('',{
+      method: 'POST',
+      body: JSON.stringify(optionsData),
+      headers: {'X-CSRFToken': csrftoken, 'content-type':'application/json'}
+    })
+    .then(data => {
+      if (data.status == '200') {
+        location.reload()
+        document.querySelector(".preload").classList.remove('activate')
+      }
+    })
+  })
+}
+
+// INITIALIZE MAPBOX OPTIONS
 mapboxgl.accessToken = JSON.parse(document.getElementById('token').textContent).token;
 var initialZoom = 9;
 var initialCenter = [currentLocation.longitude, currentLocation.latitude]
@@ -219,7 +250,7 @@ map.on('style.load', function() {
     var features = map.queryRenderedFeatures(e.point, {
         layers: ['clusters'],
     });
-    
+
     // properties fo selected feature
     let selectedFeat = e.features[0]
 
@@ -269,15 +300,22 @@ map.on('style.load', function() {
         <div class='col s6'><span style='color: #ff5c5c;'>${selectedFeat.properties.num_of_artists}</span></div>
       </div>
       <div class='row valign-wrapper'>
-        <div class='col s6'><p>Popular Genres:</p></div>
+        <div class='col s6'><p>Common Genres:</p></div>
         <div class='col s6'><span class='primary'>${selectedFeat.properties.top_genres}</span></div>
       </div>
     `
     // ARTIST FEATURES
     artist_html = ``
+
+    // this function constructs the side panel based on the map mode
     builder = (artist) => {
+
+      // discovery will look for the latest releases and display a player to preview artists
       if (artist.latest_release) {
+
+        // checks to see if the release is a track or album
         if (artist.latest_release.type == 'track') {
+
           artist_html += `
           <li>
             <div class='collapsible-header'><h6>${artist.name}</h6></div>
@@ -296,6 +334,7 @@ map.on('style.load', function() {
           </li>
           `
         } else if (artist.latest_release.type == 'album') {
+
           artist_html += `
           <li>
             <div class='collapsible-header'><h6>${artist.name}</h6></div>
@@ -314,7 +353,10 @@ map.on('style.load', function() {
           </li>
           `
         }
+
+      // if the artist doesn't have a release ID then no player is shown
       } else {
+
           artist_html += `
           <li>
             <div class='collapsible-header'><h6>${artist.name}</h6></div>
@@ -329,21 +371,15 @@ map.on('style.load', function() {
             </div>
           </li>
           `
-      }
-    };
-    artists.forEach(builder);
-
-    // PAGINATION
-    // function paginate() {
-    //   pageSize = 25
-    //   totalPages = getNumberOfPages()
-    //   for (i=1; i <= totalPages; i++) {
-
-    //   }
-    // }
-    // function getNumberOfPages() {
-    //   return Math.ceil(artists.length / 25)
-    // }
+        }
+        /*
+          THIS NEEDS TO BE CHANGED FROM A COLLAPSIBLE TO SIMPLE ROWS + 2 COLUMNS (name/link & genre)
+          in this mode, there is no genre filter, which means there are thousands of artists in the GeoJSON file.
+          to make this mode run more smoothly (and to keep from making thousands of requests to bandcamp for the preview players),
+          the previews are not included.
+        */
+    }
+    artists.forEach(builder)
 
     // OPEN SIDENAV
     document.querySelector('#city').innerHTML = city_html
@@ -352,7 +388,7 @@ map.on('style.load', function() {
     document.querySelector('#sideFeats').style.width = '300px'
     document.querySelector('#map-container').style.left = '300px'
 
-  });
+  })
 
   // listen for the mouse moving over the map and react when the cursor is over our data
   map.on('mouseenter','clusters', function (e) {
@@ -360,42 +396,42 @@ map.on('style.load', function() {
     // query for the features under the mouse, but only in the lots layer
     var features = map.queryRenderedFeatures(e.point, {
         layers: ['clusters'],
-    });
+    })
 
     map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
 
     // coords and feats for popup and sidebar info
-    var coordinates = features[0].geometry.coordinates.slice();
-    let selectedFeat = e.features[0];
-    
+    var coordinates = features[0].geometry.coordinates.slice()
+    let selectedFeat = e.features[0]
+
     let popup_html = `
       <div class='popup-feature'>
         <div class='popup-header'>
           <h4>${selectedFeat.properties.city}</h4><span class='artist-count'>${selectedFeat.properties.num_of_artists} artists</span>
         </div>
         <div class='custom-content'>
-          Most Popular Genres:
+          Most Common Genres:
         </div>
         <div class='top-genres'><span class='primary'><em>${selectedFeat.properties.top_genres}</em></span></div>
       </div>
-    `;
+    `
 
     // filling in the empty source for the highlighted city
     map.getSource('feature-highlight').setData(selectedFeat);
 
     // popup for hovering over city
     popup.setLngLat(coordinates).setHTML(popup_html).addTo(map);
-  });
+  })
 
-  
+
   map.on('mouseleave', 'clusters', function () {
-    map.getCanvas().style.cursor = '';
+    map.getCanvas().style.cursor = ''
     // popup.remove();
 
     // reset the highlight source to an empty featurecollection
     map.getSource('feature-highlight').setData({
       type: 'FeatureCollection',
       features: []
-    });
-  }); 
-});
+    })
+  })
+})
--- frontpage/forms.py
+++ frontpage/forms.py
@@ -1,14 +1,7 @@
 from django import forms
-from frontpage.models import SessionMap
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='name', max_length=100)
     subject = forms.CharField(label='subject', max_length=200)
     sender = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
-
-
-class FilterForm(forms.ModelForm):
-    class Meta:
-        model = SessionMap
-        fields = ['genre1','genre2']
